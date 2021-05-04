625ae5ace87cc0cfc6689fe48eec64da7f0e4f77
Grace Cramer <grace_cramer1@baylor.edu>
2021-05-04 15:41:45 -0500

    almost doneeee

--- pizzerias/models.py
+++ pizzerias/models.py
@@ -22,3 +22,13 @@ class Topping(models.Model):
 
         def __str__(self):
             return f"{self.text[:50]}..."
+
+
+"""class Comment(models.Model):
+    post = models.ForeignKey(Name, on_delete=models.CASCADE)
+    text = models.CharField(max_length=200)
+    date_added = models.DateTimeField(auto_now_add=True, blank=True)
+
+    def __str__(self):
+        return self.text
+"""
